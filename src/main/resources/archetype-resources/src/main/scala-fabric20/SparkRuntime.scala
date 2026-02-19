86b0226abc90fc82e60d4326dd362d2d2be8fb7f
Aravind Yarram <yaravind@gmail.com>
2026-02-18 21:50:01 -0500

    Remove deuplicated code from postbuild.groovy to shared file. Add logging

--- src/main/resources/archetype-resources/src/main/scala-fabric20/SparkRuntime.scala
+++ src/main/resources/archetype-resources/src/main/scala-fabric20/SparkRuntime.scala
@@ -1,8 +1,11 @@
 package ${package}
 
+import org.apache.logging.log4j.LogManager
 import org.apache.spark.sql.SparkSession
 
 object SparkRuntime {
+  private val logger = LogManager.getLogger(getClass)
+
   def session(appName: String): SparkSession = {
     val spark = SparkSession
       .builder()
@@ -16,19 +19,19 @@ object SparkRuntime {
 
   def printVersions(): Unit = {
     val spark = SparkSession.getActiveSession.getOrElse(SparkSession.active)
-    println("Spark version:\t\t\t\t\t " + spark.version)
-    println("Delta version:\t\t\t\t\t " + io.delta.VERSION)
-    println("Scala version:\t\t\t\t\t " + util.Properties.versionNumberString)
-    println("Java version:\t\t\t\t\t " + System.getProperty("java.version"))
-    println("JRE name:\t\t\t\t\t\t " + sys.props.get("java.runtime.name").getOrElse("**Not Available**"))
-    println("JRE version:\t\t\t\t\t " + sys.props.get("java.runtime.version").getOrElse("**Not Available**"))
-    println("java.vendor:\t\t\t\t\t " + System.getProperty("java.vendor"))
-    println("os.name:\t\t\t\t\t\t " + sys.props.get("os.name").getOrElse("**Not Available**"))
-    println("Metastore version:\t\t\t " + getSparkConfProp("spark.sql.hive.metastore.version"))
-    println("Catalog/Metastore type:\t\t " + getSparkConfProp("spark.sql.catalogImplementation"))
-    // println("Spark warehouse location: "+ getSparkConfProp("spark.sql.warehouse.dir"))
-    println("user.timezone:\t\t\t\t\t " + sys.props.get("user.timezone").getOrElse("**Not Available**"))
-    println("spark.sql.session.timeZone:\t " + getSparkConfProp("spark.sql.session.timeZone"))
+    logger.debug("Spark version:\t\t\t\t\t " + spark.version)
+    logger.debug("Delta version:\t\t\t\t\t " + io.delta.VERSION)
+    logger.debug("Scala version:\t\t\t\t\t " + util.Properties.versionNumberString)
+    logger.debug("Java version:\t\t\t\t\t " + System.getProperty("java.version"))
+    logger.debug("JRE name:\t\t\t\t\t\t " + sys.props.get("java.runtime.name").getOrElse("**Not Available**"))
+    logger.debug("JRE version:\t\t\t\t\t " + sys.props.get("java.runtime.version").getOrElse("**Not Available**"))
+    logger.debug("java.vendor:\t\t\t\t\t " + System.getProperty("java.vendor"))
+    logger.debug("os.name:\t\t\t\t\t\t " + sys.props.get("os.name").getOrElse("**Not Available**"))
+    logger.debug("Metastore version:\t\t\t " + getSparkConfProp("spark.sql.hive.metastore.version"))
+    logger.debug("Catalog/Metastore type:\t\t " + getSparkConfProp("spark.sql.catalogImplementation"))
+    // logger.debug("Spark warehouse location: " + getSparkConfProp("spark.sql.warehouse.dir"))
+    logger.debug("user.timezone:\t\t\t\t\t " + sys.props.get("user.timezone").getOrElse("**Not Available**"))
+    logger.debug("spark.sql.session.timeZone:\t " + getSparkConfProp("spark.sql.session.timeZone"))
   }
 
   private def getSparkConfProp(key: String): String = {
